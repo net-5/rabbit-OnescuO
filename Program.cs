f311b13f68d0222326c30b1deec3a3e6df268366
OnescuO <onescu.oana@gmail.com>
2019-06-26 14:28:20 +0300

    tema-2

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use Human class
+            Human oana = new Human();//Oana is the object, human is the class
+            oana.Eyes = 2;// setter
+            oana.Name = "Oana";// setter
+            oana.SaySomething();
+            oana.Yell();
+
+            var myName = oana.Name; // getter
+            Console.WriteLine($"myName = {myName}");
+
+            // Another Human
+
+            Human otherHuman = new Human();
+            otherHuman.CanYell = true;
+            otherHuman.SaySomething();
+            otherHuman.Yell();
+
+            // Third Human
+            Human thirdHuman = new Human(2, "Scoala Informala");
+            thirdHuman.SaySomething();
+
+
+            //First dog
+            Dog firstDog = new Dog(4, "Bobita");
+            //new Dog()
+            firstDog.Bark();
+            Dog secondDog = new Dog();
+            secondDog.Bark();
+            secondDog.SaySomething();
+
+            //First car
+            Car firstCar = new Car();
+            firstCar.Name = "Ford";
+            firstCar.Weels = 4;
+            firstCar.NumberOfDoors = 6;
+            firstCar.SayCarName();
+            Console.WriteLine($"{firstCar.Name} has {firstCar.Weels} wheels and {firstCar.NumberOfDoors} doors");
+            //Print const and readonly
+            Console.WriteLine($"dateOfBirth = {firstCar.dateOfBirth}");
+
+            //third dog
+            Dog thirdDog = new Dog()
+            {
+                Legs = 4,
+                Name = "Azorel"
+
+            };
+            Console.WriteLine($"thirdDog name = {thirdDog.Name}");
+            Console.WriteLine(thirdDog.DogInformation);
+
+            //static
+            Console.WriteLine($"My static animal is {Animal.GetName()}");
+            Console.WriteLine($"My static animal is {Animal.Name}");
+
+            //structss
+            //Color color = new Color();
+           // color.red = "Red";
+            //Console.WriteLine($"I like color{color.red}");
+            //Enum
+            //Console.WriteLine($"Week Monday = {week.Monday}");
+            //Console.WriteLine($"Week Monday = {(int)week.Monday}");
+            //Console.WriteLine($"Week Tuesday = {(int)week.Tuesday}");
+
+            //i get
+
+            Vehicle vehicle = new Vehicle();
+            vehicle.Name = "Ford";
+            vehicle.Country = "USA";
+            vehicle.Wheels = 4;
+            vehicle.Price = 20000;
+            vehicle.CreationDate = 2015;
+            vehicle.GetCarInfo();
+            Console.WriteLine(vehicle.CarAge);
+            vehicle.GetCarPrice();
+
+            //Rabbit
+            Rabbit rabbit = new Rabbit(EyesType.Blue, FurType.White,GenderType.Male, 2012, "mammal", "moves, sleep and eats");
+            Console.WriteLine(rabbit.GetRabbitInfo);
+            rabbit.BirthDate = 2012;
+            rabbit.Behavior = "moves, sleeps and eats";
+            rabbit.Type = "mammal";
+            
+            Console.WriteLine(rabbit.RabbitAge);
+            rabbit.RabbitBehavior();
+
+
+        }
+
+
+
+
+        //struct Color
+        //{
+          //  public string red;
+            //public string green;
+        //}
+        //struct Square
+        //{
+          //  public int area;
+            //public Color borderColor;
+        //}
+        //enum week
+        //{
+          //  Monday = 3,
+           // Tuesday = 0,
+           // Wednesday = 23,
+           // Thursday = 20,
+           // Saturday,
+            //Sunday
+                       
+       // }
+
+    }
+
+    }
+
+ 
+
+    
+    
+    
+
---
+++ Rabbit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public enum EyesType
+    {
+        Blue,
+        Red,
+        Black
+    }
+    public enum FurType
+    {
+        White,
+        Brown,
+        Black,
+        Grey
+    }
+    public enum GenderType
+    {
+        Male,
+        Female
+    }
+    public class Rabbit
+    {
+        //a rabbit can have Blue, Red or Black eyes
+        //a rabbit's fur can be white, brown, black or grey
+        //it has a gender
+        //has a birth date, and based on that you should be able to see how old the rabbit is
+        //we also know that a rabbit is a mammal that moves, sleeps and eats
+        
+        public int BirthDate { get; set; }
+        public string Type { get; set; }
+        public string Behavior { get; set; }
+        public Rabbit(EyesType eyes, FurType fur, GenderType gender, int birthDate, string type, string behavior)
+        {
+            eyes = eyes;
+            fur = fur;
+            gender = gender;
+            BirthDate = birthDate;
+            Type = type;
+            Behavior = behavior;
+
+        }
+
+
+        private int CurrentYear = 2019;
+        
+        public string GetRabbitInfo
+        {
+            get
+            {
+                return $"The rabbit has { EyesType.Blue} eyes, { FurType.White} fur and it is a { GenderType.Male }";
+            }
+        }
+        public int Age
+        {
+            get { return this.CurrentYear - this.BirthDate; }
+        }
+        public string RabbitAge
+        {
+            get { return $"The rabbit is {this.Age} years old."; }
+        }
+        public void RabbitBehavior()
+        {
+            Console.WriteLine($"The rabbit is a {this.Type} and he likes to {this.Behavior}.");
+        }
+    }
+}
